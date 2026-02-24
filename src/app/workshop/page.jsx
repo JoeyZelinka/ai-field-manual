084857ac93e7be2944cb4de2a51be724cdaff71b
Joey Zelinka <cheflife365@gmail.com>
2026-02-24 17:18:36 -0500

    try again

--- src/app/workshop/page.jsx
+++ src/app/workshop/page.jsx
@@ -1,6 +1,15 @@
 // src/app/workshop/page.jsx
+import { Suspense } from "react";
 import WorkshopClient from "./WorkshopClient";
 
 export default function Page() {
-  return <WorkshopClient />;
+  return (
+    <Suspense
+      fallback={
+        <div style={{ minHeight: "100vh", background: "rgb(18,10,12)" }} />
+      }
+    >
+      <WorkshopClient />
+    </Suspense>
+  );
 }
